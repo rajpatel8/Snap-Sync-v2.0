0e921af033c3eb798b52e2fa1cf4e03078848d9e
Lord Rajkumar <rajphx299@gmail.com>
2025-04-10 14:41:45 -0400

    Refactor .gitignore and enhance w25clients.c with command handling and file validation

--- w25clients.c
+++ w25clients.c
@@ -7,20 +7,24 @@
 #include <netinet/in.h>
 #include <netdb.h> 
 
-void error(const char *msg)
-{
-    perror(msg);
-    exit(0);
-}
+#define bool int
+#define true 1
+#define false 0
+
+bool is_valid_file(const char *filename) ;
+
+int sanitize_input(char * buffer); 
+
+void error(const char *msg);
 
 int main(int argc, char *argv[])
 {
-    signal(SIGCHLD,SIG_IGN) ;
+    // signal(SIGCHLD,SIG_IGN) ;
     int sockfd, portno, n;
     struct sockaddr_in serv_addr;
     struct hostent *server;
 
-    char buffer[3];
+    char buffer[256];
     if (argc < 3) {
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
@@ -51,16 +55,82 @@ int main(int argc, char *argv[])
     if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
 
-    printf("Please enter the File details to be uploaded :");
+    while (1)
+    {
+        printf("Please enter Command : \n");
 
-    bzero(buffer,3);
-    fgets(buffer,3,stdin);
+        bzero(buffer,256);
+        fgets(buffer,256,stdin);
 
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    
+        sanitize_input(buffer) ;
+
+    /*
+    TODO : uploadf filename deatination_path
+           downlf filename
+           removef fielname
+           dispfname pathname 
+            - c , pdf , txt and zip files
+            - In-oder, chronlogical order
+            
+    */
+
+
+    }
+        
     close(sockfd);
     return 0;
+}
+
+int sanitize_input(char * buffer){
+    // check the first word, and set the function bit accordingly
+    int mode_bit = 0; // default mode is disabled
+    char *pch;
+    pch = strtok (buffer," ");
+    mode_bit = (strcasecmp(pch,"uploadf") == 0) ? 1 : 
+              (strcasecmp(pch,"downlf") == 0) ? 2 : 
+              (strcasecmp(pch,"removef") == 0) ? 3 : 
+              (strcasecmp(pch,"dispfname") == 0) ? 4 : 
+              (strcasecmp(pch,"exit") == 0) ? 5 : -1;
+
+    // Now caheck the rest of the string accordingly
+    switch (mode_bit)
+    {
+        case -1:
+            printf("Invalid command\n");
+            return 0;
+        
+        case 1:
+            // uploadf, check if the filename file exists in pwd
+            pch = strtok (NULL, " ");
+            if (pch == NULL) {
+                printf("Please provide a filename\n");
+                break;
+            }
+            if (is_valid_file(pch)) {
+                printf("File %s exists\n", pch);
+                // get the destination path
+                char * dest = strtok (NULL, " ");
+                // upload_file(pch, dest);
+            } else {
+                printf("File %s does not exist\n", pch);
+            }
+            break;
+            
+            
+    }
+}
+
+bool is_valid_file(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file) {
+        fclose(file);
+        return true;
+    }
+    return false;
+}
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
 }
